9638076d446968bb40f35af376b9abb8bb87d4eb
RodrigoAguin <00172426@uca.edu.sv>
2026-06-25 20:57:29 -0600

    randomizador

--- Proyecto_Juego/funciones.cpp
+++ Proyecto_Juego/funciones.cpp
@@ -1,8 +1,18 @@
 #include "funciones.h"
 #include <iostream>
+#include <random>
+#include <ctime>
 
 using namespace std;
 //Funciones
+
+int numeroRandom (int randNum1, int randNum2){
+    unsigned rand = time(nullptr);
+    mt19937 nrandom(rand);
+    uniform_int_distribution<int> distribucion(randNum1, randNum2);
+    
+    return distribucion(nrandom);
+}
 
 void menuPrincipal ()
 {
